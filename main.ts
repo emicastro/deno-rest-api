c9e954d9607d95b66dc2cb95db2e432088bc7ec9
Emilio Castro <emilio.castro@mercadolibre.com>
2020-07-23 13:39:54 -0300

    using environment port variable to set the port where api is running

--- main.ts
+++ main.ts
@@ -1,9 +1,11 @@
 import { App } from './deps.ts'
 import { api } from './api.ts'
 
+const env = Deno.env.toObject()
 const app = new App()
+const PORT = parseInt(env.PORT) || 3000
 
 app.use('/api', api)
 
-app.listen({ port: 3000 })
-console.log('Server running on http://localhost:3000')
+app.listen({ port: PORT })
+console.log(`Server running on http://localhost:${PORT}`)
