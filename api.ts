0c7959fcc8b0564d7ea91bc70cbb8f6bef61c5a2
Emilio Castro <emilio.castro@mercadolibre.com>
2020-07-23 13:41:10 -0300

    Adding type check to router

--- api.ts
+++ api.ts
@@ -1,12 +1,12 @@
-import { Router } from './deps.ts'
+import { Router, Request, Response } from './deps.ts'
 
 const api = new Router()
 
-api.get('/', (req, res) => {
+api.get('/', (req: Request, res: Response) => {
   res.status(200).send({ status: 'Deno running!' })
 })
 
-api.get('/:id', (req, res) => {
+api.get('/:id', (req: Request, res: Response) => {
   res.status(200).send({ params: req.params.id })
 })
 
